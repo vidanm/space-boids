7898eb57269fc5b44d50d43855954870c956d90d
vidanm <vidan.tauriff@gmail.com>
2021-07-05 15:55:21 +0200

    add: rotation + better movements + masking

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ document.body.appendChild(app.view);
 //Loading Resources
 loader
 	.add("boid", "https://i.ibb.co/cgvM29P/boid.png")
-	.add("background", "https://unsplash.com/random")
+	.add("background", "https://i.ibb.co/0MKCW8n/bg.jpg")
 	.load(setup);
 
 let boid, bg;
@@ -29,20 +29,27 @@ let state;
 
 // Display
 function setup(){
+	
+	let blurFilter = new PIXI.filters.BlurFilter();
+	blurFilter.blur = 0;
 
 	bg = new Sprite(resources.background.texture);
-
+	bg.filters = [blurFilter];
+	bg.scale.x = 1.5
+	bg.scale.y = 2
 
 	boid = new Sprite(resources.boid.texture);
 	boid.anchor.set(0.5);
-	boid.x = app.screen.width / 2;
+	boid.x = 0;
 	boid.y = app.screen.height / 2;
 	boid.scale.x = 0.5;
 	boid.scale.y = 0.5;
 
+	
 	state = play;
 	app.stage.addChild(bg);
 	app.stage.addChild(boid);
+	bg.mask = boid;
 	app.ticker.add(delta => boidLoop(delta));
 }
 
@@ -50,10 +57,12 @@ function boidLoop(delta){
 	state(delta);
 }
 
-function play(delta){
-	boid.vx = 1*delta;
-	boid.vy = 1*delta;
+function move(object, distance) {
+	object.x = object.x + distance * Math.cos(object.rotation-Math.PI/2);
+	object.y = object.y + distance * Math.sin(object.rotation-Math.PI/2);
+}
 
-	boid.x += boid.vx;
-	boid.y += boid.vy;
+function play(delta){
+	move(boid, 2);
+	boid.rotation += delta * 0.01;
 }
